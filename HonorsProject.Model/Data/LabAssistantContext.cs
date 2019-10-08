c97796324771f6761e115ab82b21a753fd9aab3f
Bodovix <stbbap@gmail.com>
2019-10-08 18:05:35 +0100

    Migrations Enabled and setup for Model project

--- HonorsProject.Model/Data/LabAssistantContext.cs
+++ HonorsProject.Model/Data/LabAssistantContext.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace HonorsProject.Model.Data
 {
-    public class LabAssistantContext : DbContext
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class LabAssistantContext : DbContext
     {
-        //define tables
-        public LabAssistantContext() : base("name=DefaultConnection")
+        public LabAssistantContext()
+            : base("name=LabAssistantContext")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
         }
     }
