ab44a874e5593427e357cc58b0f562e8ff39714f
Bodovix <stbbap@gmail.com>
2019-10-08 17:28:33 +0100

    Entity Framework nuget package added

---
+++ HonorsProject.Model/Data/LabAssistantContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonorsProject.Model.Data
+{
+    public class LabAssistantContext : DbContext
+    {
+        //define tables
+        public LabAssistantContext() : base("name=DefaultConnection")
+        {
+        }
+    }
+}
