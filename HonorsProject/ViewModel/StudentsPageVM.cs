4c9e5fa7f7e364bfcb41e0c1ea3c863015c676fa
Bodovix <stbbap@gmail.com>
2019-12-09 13:14:50 +0000

    StudentPAgeVM Added

---
+++ HonorsProject/ViewModel/StudentsPageVM.cs
@@ -0,0 +1,65 @@
+﻿using HonorsProject.Model.Entities;
+using HonorsProject.ViewModel.CoreVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonorsProject.ViewModel
+{
+    public class StudentsPageVM : BaseViewModel
+    {
+        private Lecturer _lecturer;
+
+        public Lecturer Lecturer
+        {
+            get { return _lecturer; }
+            set
+            {
+                _lecturer = value;
+                OnPropertyChanged(nameof(Lecturer));
+            }
+        }
+
+        private string _searchStudentTxt;
+
+        public string SearchStudentTxt
+        {
+            get { return _searchStudentTxt; }
+            set
+            {
+                _searchStudentTxt = value;
+                OnPropertyChanged(nameof(SearchStudentTxt));
+            }
+        }
+
+        private Student _selectedStudent;
+
+        public Student SelectedStudent
+        {
+            get { return _selectedStudent; }
+            set
+            {
+                _selectedStudent = value;
+                OnPropertyChanged(nameof(SelectedStudent));
+            }
+        }
+
+        private List<Student> _students;
+
+        public List<Student> Students
+        {
+            get { return _students; }
+            set
+            {
+                _students = value;
+                OnPropertyChanged(nameof(Students));
+            }
+        }
+
+        public StudentsPageVM(string dbcontextName) : base(dbcontextName)
+        {
+        }
+    }
+}
