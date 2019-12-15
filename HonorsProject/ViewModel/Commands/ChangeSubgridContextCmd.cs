5d4d2151b6106c539ae17a9c4526f1a9b8e28505
Bodovix <stbbap@gmail.com>
2019-12-15 15:51:39 +0000

    Single Chage Subgrid Conetxt command added. need to refactor out old commands and implement questions subgrid

---
+++ HonorsProject/ViewModel/Commands/ChangeSubgridContextCmd.cs
@@ -0,0 +1,35 @@
+﻿using HonorsProject.Model.Enums;
+using HonorsProject.ViewModel.Commands.IComands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HonorsProject.ViewModel.Commands
+{
+    public class ChangeSubgridContextCmd : ICommand
+    {
+        public IChangeSubgridCmd VM { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public ChangeSubgridContextCmd(IChangeSubgridCmd vm)
+        {
+            VM = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            //no pre checks needed
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            SubgridContext context = (SubgridContext)parameter;
+            VM.ChangeSubgridContext(context);
+        }
+    }
+}
---
+++ HonorsProject/ViewModel/Commands/IComands/IChangeSubgridCmd.cs
@@ -0,0 +1,16 @@
+﻿using HonorsProject.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonorsProject.ViewModel.Commands.IComands
+{
+    public interface IChangeSubgridCmd
+    {
+        ChangeSubgridContextCmd GetChangeSubgridContextCmd { get; set; };
+
+        bool ChangeSubgridContext(SubgridContext context);
+    }
+}
