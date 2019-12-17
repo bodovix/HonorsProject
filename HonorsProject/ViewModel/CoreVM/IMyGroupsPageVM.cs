4a737ed8c00598c4d8950acc0f8d558121fc8e9b
Bodovix <stbbap@gmail.com>
2019-12-17 17:19:55 +0000

    MyGroupsVM properties implemented

--- HonorsProject/ViewModel/CoreVM/IMyGroupsPageVM.cs
+++ HonorsProject/ViewModel/CoreVM/IMyGroupsPageVM.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HonorsProject.Model.Entities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,8 @@ namespace HonorsProject.ViewModel.CoreVM
 {
     public interface IMyGroupsPageVM
     {
+        Group SelectedGroup { get; set; }
+        ObservableCollection<Group> Groups { get; set; }
+        string GroupSearchTxt { get; set; }
     }
 }
--- HonorsProject/ViewModel/MyGroupsStudentPageVM.cs
+++ HonorsProject/ViewModel/MyGroupsStudentPageVM.cs
@@ -1,17 +1,59 @@
 ﻿using HonorsProject.Model.Core;
+using HonorsProject.Model.Entities;
 using HonorsProject.Model.Enums;
 using HonorsProject.Model.HelperClasses;
 using HonorsProject.ViewModel.CoreVM;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HonorsProject.ViewModel
 {
-    internal class MyGroupsStudentPageVM : BaseViewModel, IMyGroupsPageVM
+    public class MyGroupsStudentPageVM : BaseViewModel, IMyGroupsPageVM
     {
+        #region Properties
+
+        private Group _selectedGroup;
+
+        public Group SelectedGroup
+        {
+            get { return _selectedGroup; }
+            set
+            {
+                _selectedGroup = value;
+                OnPropertyChanged(nameof(SelectedGroup));
+            }
+        }
+
+        private ObservableCollection<Group> _groups;
+
+        public ObservableCollection<Group> Groups
+        {
+            get { return _groups; }
+            set
+            {
+                _groups = value;
+                OnPropertyChanged(nameof(Groups));
+            }
+        }
+
+        private string _groupSearchTxt;
+
+        public string GroupSearchTxt
+        {
+            get { return _groupSearchTxt; }
+            set
+            {
+                _groupSearchTxt = value;
+                OnPropertyChanged(nameof(GroupSearchTxt));
+            }
+        }
+
+        #endregion Properties
+
         public MyGroupsStudentPageVM(ISystemUser appUser, string dbcontextName) : base(dbcontextName)
         {
         }
