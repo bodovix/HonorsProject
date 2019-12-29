a2e45f4aa380c74bc8508208c5389662beb6a6b5
Bodovix <stbbap@gmail.com>
2019-12-29 15:21:02 +0000

    GoToSessionCommand Created

---
+++ HonorsProject/ViewModel/Commands/GoToSessionCmd.cs
@@ -0,0 +1,35 @@
+﻿using HonorsProject.Model.Entities;
+using HonorsProject.ViewModel.Commands.IComands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HonorsProject.ViewModel.Commands
+{
+    public class GoToSessionCmd : ICommand
+    {
+        public IGoToSessionCmd VM { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public GoToSessionCmd(IGoToSessionCmd vm)
+        {
+            VM = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            //no pre checks needed
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Session session = (Session)parameter;
+            VM.GoToSession(session);
+        }
+    }
+}
---
+++ HonorsProject/ViewModel/Commands/IComands/IGoToSessionCmd.cs
@@ -0,0 +1,15 @@
+﻿using HonorsProject.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonorsProject.ViewModel.Commands.IComands
+{
+    public interface IGoToSessionCmd
+    {
+        GoToSessionCmd GoToSessionCmd { get; set; }
+        bool GoToSession(Session session);
+    }
+}
