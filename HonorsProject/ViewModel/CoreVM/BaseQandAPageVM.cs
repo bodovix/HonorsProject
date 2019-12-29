e61c4dd9f2a34c6e38dd41cb076d12c0dfd1a086
Bodovix <stbbap@gmail.com>
2019-12-29 13:52:57 +0000

    BaseQandAPageVM setup

---
+++ HonorsProject/ViewModel/CoreVM/BaseQandAPageVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonorsProject.ViewModel.CoreVM
+{
+    public abstract class BaseQandAPageVM : BaseViewModel
+    {
+        public BaseQandAPageVM(string dbcontextName) : base(dbcontextName)
+        {
+        }
+
+    }
+}
