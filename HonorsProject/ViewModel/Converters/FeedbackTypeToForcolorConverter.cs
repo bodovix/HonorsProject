c3096b142df95090f75cf7dd619ec328c341d735
Bodovix <stbbap@gmail.com>
2020-01-11 12:42:37 +0000

    Feedback type color converter added

---
+++ HonorsProject/ViewModel/Converters/FeedbackTypeToForcolorConverter.cs
@@ -0,0 +1,41 @@
+﻿using HonorsProject.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HonorsProject.ViewModel.Converters
+{
+    internal class FeedbackTypeToForcolorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            FeedbackType feedbackType = (FeedbackType)value;
+
+            switch (feedbackType)
+            {
+                case FeedbackType.Error:
+                    return Brushes.Red;
+
+                case FeedbackType.Info:
+                    return Brushes.Blue;
+
+                case FeedbackType.Success:
+                    return Brushes.Green;
+
+                default:
+                    throw new Exception("Invalid feedback type for converter. Please contact support.");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Convert Back not required");
+        }
+    }
+}
