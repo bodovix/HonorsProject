13afcdcfe7c7fac6751320a42069901d5cd56a99
Bodovix <stbbap@gmail.com>
2020-01-17 09:45:31 +0000

    bug fixes and addtional checks in load session for specific question (InSessionLecturerVM)

--- HonorsProject/ViewModel/InSessoinLecturerQandAVM.cs
+++ HonorsProject/ViewModel/InSessoinLecturerQandAVM.cs
@@ -21,21 +21,43 @@ namespace HonorsProject.ViewModel
     {
         public InSessoinLecturerQandAVM(ISystemUser appUser, Session selectedSession, string dbcontextName) : base(appUser, dbcontextName)
         {
-            //Setup
-            FormContextAnswer = FormContext.Create;
-            SelectedSession = selectedSession;//Might need to attach this to the UoW. not sure yet
-            Questions = new ObservableCollection<Question>(UnitOfWork.QuestionRepository.GetFromSession(SelectedSession).ToList());
-            ///Answers loaded when question selected
+            try
+            {
+                //Setup
+                FormContextAnswer = FormContext.Create;
+                SelectedSession = selectedSession;//Might need to attach this to the UoW. not sure yet
+                Questions = new ObservableCollection<Question>(UnitOfWork.QuestionRepository.GetFromSession(SelectedSession).ToList());
+                ///Answers loaded when question selected
+            }
+            catch (Exception ex)
+            {
+                ShowFeedback(ex.Message, FeedbackType.Error);
+            }
         }
 
         public InSessoinLecturerQandAVM(ISystemUser appUser, Question selectedQuestion, string dbcontextName) : base(appUser, dbcontextName)
         {
-            //Setup
-            FormContextAnswer = FormContext.Create;
-            SelectedSession = UnitOfWork.SessionRepository.GetSessionWithQuestion(selectedQuestion);//Might need to attach this to the UoW. not sure yet
-            SelectedQuestion = selectedQuestion;
-            Questions = new ObservableCollection<Question>(UnitOfWork.QuestionRepository.GetFromSession(SelectedSession).ToList());
-            ///Answers loaded when question selected
+            try
+            {
+                //Setup
+                if (selectedQuestion != null)
+                    if (selectedQuestion.Id != 0)
+                    {
+                        SelectedSession = UnitOfWork.SessionRepository.GetSessionWithQuestion(selectedQuestion);//Might need to attach this to the UoW. not sure yet
+                        SelectedQuestion = UnitOfWork.QuestionRepository.Get(selectedQuestion.Id);
+                        QandAMode = QandAMode.Question;
+                        Questions = new ObservableCollection<Question>(UnitOfWork.QuestionRepository.GetFromSession(SelectedSession).ToList());
+                        ///Answers loaded when question selected
+                    }
+                    else
+                    {
+                        ShowFeedback($"Cannot load session for question with id value: {selectedQuestion.Id}", FeedbackType.Error);
+                    }
+            }
+            catch (Exception ex)
+            {
+                ShowFeedback(ex.Message, FeedbackType.Error);
+            }
         }
 
         protected override bool UpdateQuestionsList(BaseEntity sSession, string QuestionSearchTxt)
