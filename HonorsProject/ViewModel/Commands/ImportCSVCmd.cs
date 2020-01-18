f093558134b2cb8b8a6ea44e9c84ac5994873666
Bodovix <stbbap@gmail.com>
2020-01-18 09:26:35 +0000

    Import CSV command added

---
+++ HonorsProject/ViewModel/Commands/ImportCSVCmd.cs
@@ -0,0 +1,33 @@
+﻿using HonorsProject.ViewModel.Commands.IComands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HonorsProject.ViewModel.Commands
+{
+    public class ImportCSVCmd : ICommand
+    {
+        public IImportCSVCmd VM { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public ImportCSVCmd(IImportCSVCmd vm)
+        {
+            VM = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            //no pre checks needed
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            VM.Import();
+        }
+    }
+}
