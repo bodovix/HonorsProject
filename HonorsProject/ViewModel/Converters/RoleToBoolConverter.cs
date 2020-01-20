aee3b5c790e5bc2e6d405d09eb24dae2c38fe0f8
Bodovix <stbbap@gmail.com>
2020-01-20 11:32:19 +0000

    Group page inputs now only editable for Lecturers

---
+++ HonorsProject/ViewModel/Converters/RoleToBoolConverter.cs
@@ -0,0 +1,28 @@
+﻿using HonorsProject.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HonorsProject.ViewModel.Converters
+{
+    public class RoleToBoolConverter : IValueConverter
+    {
+        public object Convert(object userRole, Type targetType, object parameter, CultureInfo culture)
+        {
+            Role paramiterToMach = (Role)parameter;
+            //if roles match required make the item visible
+            if ((Role)userRole == paramiterToMach)
+                return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Convert Back not required");
+        }
+    }
+}
