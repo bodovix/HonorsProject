2e0448a5d78dd057b6526c9d26d802773511da51
Bodovix <stbbap@gmail.com>
2020-01-20 12:23:13 +0000

    QandA page is Editable converters improved to use the DTO classes so only enabled for the owners posts

---
+++ HonorsProject/ViewModel/Converters/AnswerToUserBoolConverter.cs
@@ -0,0 +1,36 @@
+﻿using HonorsProject.Model.Entities;
+using HonorsProject.Model.Enums;
+using HonorsProject.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HonorsProject.ViewModel.Converters
+{
+    internal class AnswerToUserBoolConverter : IValueConverter
+    {
+        public object Convert(object obj, Type targetType, object parameter, CultureInfo culture)
+        {
+            AnswerStateConverterDTO dto = (AnswerStateConverterDTO)obj;
+            if (dto.Answer == null || dto.User == null)
+                return false;
+            if (dto.Answer.AnsweredBy == null)
+                return false;
+            //if roles match required make the item visible
+            if (dto.User.Id == dto.Answer.AnsweredBy.Id)
+                return true;
+            else
+                return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Convert Back not required");
+        }
+    }
+}
---
+++ HonorsProject/ViewModel/Converters/QuestionToUserBoolConverter.cs
@@ -0,0 +1,37 @@
+﻿using HonorsProject.Model.Entities;
+using HonorsProject.Model.Enums;
+using HonorsProject.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HonorsProject.ViewModel.Converters
+{
+    internal class QuestionToUserBoolConverter : IValueConverter
+    {
+        public object Convert(object obj, Type targetType, object parameter, CultureInfo culture)
+        {
+            QuestionStateConverterDTO dto = (QuestionStateConverterDTO)obj;
+            if (dto.Question == null || dto.User == null)
+                return false;
+            if (dto.Question.AskedBy == null)
+                return false;
+
+            //if roles match required make the item visible
+            if (dto.User.Id == dto.Question.AskedBy.Id)
+                return true;
+            else
+                return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Convert Back not required");
+        }
+    }
+}
