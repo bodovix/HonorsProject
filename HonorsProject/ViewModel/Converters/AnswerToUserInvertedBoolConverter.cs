bc362711b183c4ac3dbdffbdb53f1bb2dabc9080
Bodovix <stbbap@gmail.com>
2020-01-25 11:38:36 +0000

    Inverted Bool converter added for QandA users converters - so i can do isreadonly converters

---
+++ HonorsProject/ViewModel/Converters/AnswerToUserInvertedBoolConverter.cs
@@ -0,0 +1,36 @@
+﻿using HonorsProject.Model.Entities;
+using HonorsProject.Model.Enums;
+using HonorsProject.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HonorsProject.ViewModel.Converters
+{
+    internal class AnswerToUserInvertedBoolConverter : IValueConverter
+    {
+        public object Convert(object obj, Type targetType, object parameter, CultureInfo culture)
+        {
+            AnswerStateConverterDTO dto = (AnswerStateConverterDTO)obj;
+            if (dto.Answer == null || dto.User == null)
+                return true;
+            if (dto.Answer.AnsweredBy == null)
+                return true;
+            //if roles match required make the item visible
+            if (dto.User.Id == dto.Answer.AnsweredBy.Id)
+                return false;
+            else
+                return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Convert Back not required");
+        }
+    }
+}
---
+++ HonorsProject/ViewModel/Converters/QuestionToUserInvertedBoolConverter.cs
@@ -0,0 +1,37 @@
+﻿using HonorsProject.Model.Entities;
+using HonorsProject.Model.Enums;
+using HonorsProject.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HonorsProject.ViewModel.Converters
+{
+    internal class QuestionToUserInvertedBoolConverter : IValueConverter
+    {
+        public object Convert(object obj, Type targetType, object parameter, CultureInfo culture)
+        {
+            QuestionStateConverterDTO dto = (QuestionStateConverterDTO)obj;
+            if (dto.Question == null || dto.User == null)
+                return true;
+            if (dto.Question.AskedBy == null)
+                return true;
+
+            //if roles match required make the item visible
+            if (dto.User.Id == dto.Question.AskedBy.Id)
+                return false;
+            else
+                return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Convert Back not required");
+        }
+    }
+}
