e85433a38093f2841ca6540cc34f85c6534d82df
Bodovix <stbbap@gmail.com>
2020-02-21 12:04:44 +0000

    TextRAzorAPI work

---
+++ HonorsProject.Test/Tests/VMTests/DataAnalysisTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using HonorsProject.Model.Core;
+using HonorsProject.Model.Data;
+using HonorsProject.ViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HonorsProject.Test.VMTest
+{
+    [TestClass]
+    public class DataAnalysisTests : BaseTest
+    {
+        private DataAnalysisVM VM;
+        private BaseEntity entityToFucusOn = null;
+
+        public DataAnalysisTests() : base()
+        {
+            VM = new DataAnalysisVM(entityToFucusOn, dbConName);
+        }
+
+        [TestMethod]
+        public void TextRazorSucces()
+        {
+            //Arrange
+
+            //Act
+            string result = VM.GetKeyPhraseAPI();
+            //Assert
+            Assert.IsTrue(!String.IsNullOrEmpty(result));
+        }
+    }
+}
--- HonorsProject/ViewModel/DataAnalysisVM.cs
+++ HonorsProject/ViewModel/DataAnalysisVM.cs
@@ -8,13 +8,15 @@ using HonorsProject.ViewModel.CoreVM;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HonorsProject.ViewModel
 {
-    internal class DataAnalysisVM : BaseViewModel, IGoToEntityCmd
+    public class DataAnalysisVM : BaseViewModel, IGoToEntityCmd
     {
         #region Properties
 
@@ -230,6 +232,30 @@ namespace HonorsProject.ViewModel
             }
             ShowFeedback("Cannot go to an unsupported object type.", FeedbackType.Error);
             return false;
+        }
+
+        public string GetKeyPhraseAPI()
+        {
+            //using the TextRazor API
+            string html = null;
+            string url = @"https://api.textrazor.com";
+            string content = "TEST: All endpoints expect the API Key to be passed as an HTTP header ";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+            request.Headers.Add("X-TextRazor-Key", "31a755e8066eebf713e7e402cca27c8c406d72f0b24a079525998cad");
+            request.Headers.Add("extractors", "entities,entailments");
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Headers.Add("text", content);
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            return html;
         }
 
         private void UpdateGroupsList()
