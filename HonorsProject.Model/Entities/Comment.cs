56c626cc6208e6cbb9a7c2f2558098867862d887
Bodovix <stbbap@gmail.com>
2020-03-04 16:04:28 +0000

    comment class added

---
+++ HonorsProject.Model/Entities/Comment.cs
@@ -0,0 +1,27 @@
+﻿using HonorsProject.Model.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonorsProject.Model.Entities
+{
+    public class Comment : BaseEntity
+    {
+        public string CommentText { get; set; }
+        public string PostedByName { get; set; }
+        public Question Quesetion { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string commentText, string postedByName, Question quesetion)
+        {
+            CommentText = commentText;
+            PostedByName = postedByName;
+            Quesetion = quesetion;
+        }
+    }
+}
