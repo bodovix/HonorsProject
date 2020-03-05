21b400354ba25da90933b4636dd8ec9d8fc35e70
Bodovix <stbbap@gmail.com>
2020-03-05 11:42:07 +0000

    visibility converter added for comment deltee buttnons

---
+++ HonorsProject/ViewModel/Converters/IDToVisConverter.cs
@@ -0,0 +1,28 @@
+﻿using HonorsProject.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HonorsProject.ViewModel.Converters
+{
+    public class IDToVisConverter : IValueConverter
+    {
+        public object Convert(object id, Type targetType, object parameter, CultureInfo culture)
+        {
+            //if roles match required make the item visible
+            if ((int)id == App.AppUser.Id)
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Convert Back not required");
+        }
+    }
+}
