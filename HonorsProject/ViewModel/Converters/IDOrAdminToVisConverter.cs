b74cd121ed150d1e82732f7191b286e286c3e72d
Bodovix <stbbap@gmail.com>
2020-03-05 13:00:21 +0000

    IDOrAdminVisConverter added

---
+++ HonorsProject/ViewModel/Converters/IDOrAdminToVisConverter.cs
@@ -0,0 +1,30 @@
+﻿using HonorsProject.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HonorsProject.ViewModel.Converters
+{
+    public class IDOrAdminToVisConverter : IValueConverter
+    {
+        public object Convert(object id, Type targetType, object parameter, CultureInfo culture)
+        {
+            //if roles match required make the item visible
+            if (App.LoggedInAs == Role.Lecturer)
+                return Visibility.Visible;
+            if ((int)id == App.AppUser.Id)
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Convert Back not required");
+        }
+    }
+}
