d3d5d241fb1661e6e28277f4f1080f8ce9ababed
Bodovix <stbbap@gmail.com>
2020-03-07 15:09:42 +0000

    inverse boolean converter added

---
+++ HonorsProject/ViewModel/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HonorsProject.ViewModel.Converters
+{
+    internal class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool boolVal = (bool)value;
+
+            if (boolVal)
+                return !boolVal;
+            return boolVal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Convert Back not required");
+        }
+    }
+}
