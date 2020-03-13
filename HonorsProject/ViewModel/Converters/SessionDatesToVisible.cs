d0e7d63cd2b43d6d6586fa33c23420b499f327d4
Bodovix <stbbap@gmail.com>
2020-03-13 10:30:06 +0000

    More obvious session now closed message displayed in QandA page

---
+++ HonorsProject/ViewModel/Converters/SessionDatesToVisible.cs
@@ -0,0 +1,31 @@
+﻿using HonorsProject.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HonorsProject.ViewModel.Converters
+{
+    internal class SessionDatesToVisible : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Session session = value as Session;
+            if (session == null)
+                return Visibility.Collapsed;
+            //if session is Active don't show the label
+            if (DateTime.Now >= session.StartTime && DateTime.Now <= session.EndTime)
+                return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Convert Back not required");
+        }
+    }
+}
