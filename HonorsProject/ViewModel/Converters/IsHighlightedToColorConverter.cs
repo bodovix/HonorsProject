0c9e2197d87bebbba2e5681b927f2d46bb9b836a
Bodovix <stbbap@gmail.com>
2020-03-26 11:46:27 +0000

    background color converter added

---
+++ HonorsProject/ViewModel/Converters/IsHighlightedToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace HonorsProject.ViewModel.Converters
+{
+    internal class IsHighlightedToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isHighlighted = (bool)value;
+
+            if (isHighlighted)
+                return new SolidColorBrush(Colors.Yellow);
+            return new SolidColorBrush();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Convert Back not required");
+        }
+    }
+}
